8d38d6fabff3c97d63bca3068617620ef72873de
Vladimir <marvalo73@gmail.com>
2022-06-01 17:03:03 +0700

    готово

--- src/homework.java
+++ src/homework.java
@@ -1,5 +1,20 @@
 public class homework {
     public static void main (String[] args) {
-
+        // 1 задание
+        System.out.println("1 задание");
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastnameName = "Ivanov";
+        System.out.println("Ф.И.О. сотрудника – " + lastnameName + " " + firstName + " " + middleName);
+        // 2 задание
+        System.out.println("2 задание");
+        String name = lastnameName + ' ' + firstName + ' ' + middleName;
+        name = name.toUpperCase();
+        System.out.println("Данные ФИО сотрудника для заполнения отчета — " + name);
+        // 3 задание
+        System.out.println("3 задание");
+        String fullName = "Иванов Семён Семёнович";
+        fullName = fullName.replace('ё', 'е');
+        System.out.println(fullName);
     }
 }
